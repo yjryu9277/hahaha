cc0b4c422180760f1c26f294fb5a765aee3485b3
smhrd <smhrd@DESKTOP-DSDFOR4>
2020-09-11 11:23:21 +0900

    df

--- teamproject/src/kr/mem/controller/FrontController.java
+++ teamproject/src/kr/mem/controller/FrontController.java
@@ -12,7 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 public class FrontController extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 류윤정이 수정한 코드
+		String reqUrl = request.getRequestURI();
 		System.out.println("OK");
+		
 	}
 
 }
